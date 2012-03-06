4aff0d57c98f301361cdce887f478df892f5f342
Michi Lehr <mlehr@jimdo.com>
2012-03-06 13:12:19 +0100

    add labels

--- src/Jimdo/JimkanbanBundle/Form/TicketTypeType.php
+++ src/Jimdo/JimkanbanBundle/Form/TicketTypeType.php
@@ -5,20 +5,27 @@ namespace Jimdo\JimkanbanBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
 
-class TicketTypeType extends AbstractType
-{
-    public function buildForm(FormBuilder $builder, array $options)
-    {
+class TicketTypeType extends AbstractType {
+
+    public function buildForm(FormBuilder $builder, array $options) {
         $builder
-            ->add('name')
-            ->add('textColor')
-            ->add('backgroundColor')
-            ->add('isBackgroundFilled', 'checkbox')
+                ->add('name', 'text', array(
+                    'label' => 'Name'
+                ))
+                ->add('textColor', 'text', array(
+                    'label' => 'Text Color'
+                ))
+                ->add('backgroundColor', 'text', array(
+                    'label' => 'Background Color'
+                ))
+                ->add('isBackgroundFilled', 'checkbox', array(
+                    'label' => 'Fill entire background'
+                ))
         ;
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'jimdo_jimkanbanbundle_tickettypetype';
     }
+
 }
