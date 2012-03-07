67876a87d284e037d95ee7aa5fbc87532fe8a06e
Michi Lehr <mlehr@jimdo.com>
2012-03-07 14:39:36 +0100

    * adds test for TicketType Entity  * removes DefaultControlerTest  * adds TicketTypeControlerTest for List action

--- src/Jimdo/JimkanbanBundle/Tests/Entity/TicketTypeTest.php
+++ src/Jimdo/JimkanbanBundle/Tests/Entity/TicketTypeTest.php
@@ -5,15 +5,22 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Validator\ValidatorFactory;
 use Jimdo\JimkanbanBundle\Entity\TicketType; 
 
-class TicketTypeTest extends WebTestCase 
+class TicketTypeTest extends  WebTestCase 
 {
-
-    public function testSet() 
-    {
+    public function testSetValidbackgroundColor() {
         $e = new TicketType();
-        $e->setBackgroundColor("11111");
+        $e->setBackgroundColor("111111");
         $validator = ValidatorFactory::buildDefault()->getValidator();
         $errors = $validator->validate($e);
         $this->assertEquals(0, count($errors));
     }
+    
+    public function testSetInvalidbackgroundColor() {
+        $e = new TicketType();
+        $e->setBackgroundColor("GGGGGG");
+        $validator = ValidatorFactory::buildDefault()->getValidator();
+        $errors = $validator->validate($e);
+        $this->assertEquals(1, count($errors));
+    }
 }
+?>
