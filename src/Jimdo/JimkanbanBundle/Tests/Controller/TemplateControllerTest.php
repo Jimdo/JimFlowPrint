825d962a539933dc0cf663f7cc8ffda731ff32b7
robin <robin@jimdo.com>
2012-03-13 16:22:58 +0100

    small layout improvement

---
+++ src/Jimdo/JimkanbanBundle/Tests/Controller/TemplateControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: robin
+ * Date: 3/13/12
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class TemplateControllerTest {
+
+}
