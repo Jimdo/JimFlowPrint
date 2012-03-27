6add2fd881bab9beaa1e23325d66f5e12beef0d2
robin <robin@jimdo.com>
2012-03-27 11:39:25 +0200

    add printer entity

---
+++ src/Jimdo/JimkanbanBundle/Entity/Printer.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Jimdo\JimkanbanBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Jimdo\JimkanbanBundle\Entity\Printer
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Jimdo\JimkanbanBundle\Entity\PrinterRepository")
+ */
+class Printer
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string $email
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var text $notes
+     *
+     * @ORM\Column(name="notes", type="text")
+     */
+    private $notes;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set notes
+     *
+     * @param text $notes
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+    }
+
+    /**
+     * Get notes
+     *
+     * @return text 
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+}
