6015a0ecc711f3d797050f16a777fa2e5b22a5b2
robin <robin@jimdo.com>
2012-03-27 11:46:15 +0200

    generated crud for printer

---
+++ src/Jimdo/JimkanbanBundle/Form/PrinterType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jimdo\JimkanbanBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class PrinterType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('notes')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'jimdo_jimkanbanbundle_printertype';
+    }
+}
