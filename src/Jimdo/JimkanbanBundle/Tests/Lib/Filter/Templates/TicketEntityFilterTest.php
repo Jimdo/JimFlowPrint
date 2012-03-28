5fff642a315a8f5f68561efc72e3f229b1a7afe2
robin <robin@jimdo.com>
2012-03-28 13:20:17 +0200

    ticket type name must be unique

---
+++ src/Jimdo/JimkanbanBundle/Tests/Lib/Filter/Templates/TicketEntityFilterTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: robin
+ * Date: 3/28/12
+ * Time: 8:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class TicketEntityFilterTest {
+
+}
