56c546f388843815691ea3671b4d73573c7a23ef
robin <robin@jimdo.com>
2012-04-10 18:04:14 +0200

    add view template data

---
+++ src/Jimdo/JimkanbanBundle/Lib/TemplateDataView.php
@@ -0,0 +1,33 @@
+<?php
+namespace Jimdo\JimkanbanBundle\Lib;
+use Jimdo\JimkanbanBundle\Lib\TemplateData;
+use Symfony\Component\HttpFoundation\Request;
+use \Jimdo\JimkanbanBundle\Lib\Filter\FilterChain;
+use \Jimdo\JimkanbanBundle\Entity\PrinterRepository;
+ 
+class TemplateDataView extends  TemplateData {
+
+   /**
+    * @var \Jimdo\JimkanbanBundle\Entity\PrinterRepository
+    */
+   private $repository;
+
+   public function __construct(Request $request, FilterChain $filterChain, PrinterRepository $repository)
+   {
+       parent::__construct($request, $filterChain);
+       $this->repository = $repository;
+   }
+
+    protected function getData() {
+        return array_merge(
+            parent::getData(),
+            $this->getPrinters()
+        );
+    }
+
+    private function getPrinters()
+    {
+        return $this->repository->findAll();
+    }
+
+}
