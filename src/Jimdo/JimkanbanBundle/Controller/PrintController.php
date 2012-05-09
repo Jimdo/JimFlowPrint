9e7b4ecf5ef9b0fd9e157c9f7225ea87fdb2dcd5
robin <robin@jimdo.com>
2012-05-09 11:51:39 +0200

    wip, add gcp, lacks tests :(

--- src/Jimdo/JimkanbanBundle/Controller/PrintController.php
+++ src/Jimdo/JimkanbanBundle/Controller/PrintController.php
@@ -17,8 +17,13 @@ class PrintController extends Controller
         $templateDataService = $this->container->get('jimdo.template_data_view');
         $templateData = $templateDataService->getTemplateData();
 
-        $printingService = $this->get('jimdo.email_printing');
+        $printingService = $this->get('jimdo.gcp_printing');
         $printingService->doPrint($templateData['printer'], $this->generateUrl('template_print_view', $data, true));
+
+        $pdf = $this->container->get('jimdo.pdf_generator');
+        //$f = $pdf->generateFromUrl($this->generateUrl('template_print_view', $data, true));
+
+        //return new Response($f, 200, array('Content-Type' => 'application/pdf'));
 
         return new Response('{"message": "ok"}' , 200);
     }
