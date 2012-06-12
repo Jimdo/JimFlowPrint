7be53e1eca6be96144618c1985fa8bee922999e3
robin <robin@jimdo.com>
2012-06-12 13:47:25 +0200

    * solve autoloading issues, caused by composer update  * fix autoloading issue from buzz message factory by switching version to dev-master. This should not be permanent

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,11 +19,10 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
  	        new Jimdo\JimkanbanBundle\JimdoJimkanbanBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
-            new \Sensio\Bundle\BuzzBundle\SensioBuzzBundle()
+            new Sensio\Bundle\BuzzBundle\SensioBuzzBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
