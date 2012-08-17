3e5615db066dd7cdfb7dae7b073f9a28cad5e0c8
michi lehr <michi@jimdo.com>
2012-08-17 18:06:46 +0200

    removes old stuff

--- src/Jimdo/JimkanbanBundle/Lib/Printer/PrinterInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Jimdo\JimkanbanBundle\Lib\Printer;
-use \Jimdo\JimkanbanBundle\Entity\Printer;
-
-interface PrinterInterface
-{
-    public function doPrint($printerId, array $file);
-}
