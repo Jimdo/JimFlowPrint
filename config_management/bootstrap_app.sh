f50b7370ab43dc41daaa7e487a70a7420ed0a4bb
Ilya Margolin <ilya@jimdo.com>
2015-03-13 19:02:56 +0100

    do not create app/cache app/logs

--- config_management/bootstrap_app.sh
+++ config_management/bootstrap_app.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -xe
 ./bin/build_bootstrap
-mkdir -p app/cache app/logs
 php app/console doctrine:database:create || echo oh well...
 php app/console doctrine:migrations:migrate --no-interaction
 php app/console assetic:dump
