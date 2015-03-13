ebf720710c52d74e59f7bccd3e7f9ab5dbf7eb27
Ilya Margolin <ilya@jimdo.com>
2015-03-13 14:10:47 +0100

    another way to generate assets

--- config_management/bootstrap_app.sh
+++ config_management/bootstrap_app.sh
@@ -3,5 +3,6 @@
 mkdir -p app/cache app/logs
 php app/console doctrine:database:create || echo oh well...
 php app/console doctrine:migrations:migrate --no-interaction
+php app/console assetic:dump
 php app/console assets:install --symlink web/
 php app/console cache:clear --env=prod
