c8b1adf768d826bebffa9487c609b5877201d5e5
Ilya Margolin <ilya@jimdo.com>
2015-03-13 18:01:03 +0100

    clear:cache as www-data

--- config_management/bootstrap_app.sh
+++ config_management/bootstrap_app.sh
@@ -5,4 +5,4 @@ php app/console doctrine:database:create || echo oh well...
 php app/console doctrine:migrations:migrate --no-interaction
 php app/console assetic:dump
 php app/console assets:install --symlink web/
-php app/console cache:clear --env=prod
+sudo -u www-data php app/console cache:clear --env=prod
