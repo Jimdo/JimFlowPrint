08795b4d982cd677483b211f958a9aef8c636e42
Ilya Margolin <ilya@jimdo.com>
2015-03-13 12:02:25 +0100

    idempotency

--- config_management/bootstrap_app.sh
+++ config_management/bootstrap_app.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/sh -xe
 ./bin/build_bootstrap
 mkdir -p app/cache app/logs
-php app/console doctrine:database:create
+php app/console doctrine:database:create --if-not-exists
 php app/console doctrine:migrations:migrate --no-interaction
 php app/console assets:install --symlink web/
 php app/console cache:clear --env=prod
