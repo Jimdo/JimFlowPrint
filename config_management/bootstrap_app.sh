425715d256454535edd0002c087fa14d37699297
Ilya Margolin <ilya@jimdo.com>
2015-04-08 15:27:46 +0200

    Reorder stuff

--- config_management/bootstrap_app.sh
+++ config_management/bootstrap_app.sh
@@ -2,6 +2,6 @@
 ./bin/build_bootstrap
 php app/console doctrine:database:create || echo oh well...
 php app/console doctrine:migrations:migrate --no-interaction
-php app/console assetic:dump
 php app/console assets:install --symlink web/
+php app/console assetic:dump
 sudo -u www-data php app/console cache:clear --env=prod
